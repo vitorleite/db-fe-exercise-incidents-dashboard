5db0dfc564dcb93d58032433d5ce0368924f0c3c
Vitor Leite <vhsleite@gmail.com>
2026-01-15 10:41:22 +0200

    removed app actions (used to reset data)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 
 import { Incidents } from "./features/Incidents/Incidents";
-import { Button } from "./components/ui";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,20 +13,10 @@ const queryClient = new QueryClient({
 });
 
 function App() {
-  const resetData = async () => {
-    await fetch("/api/reset", {
-      method: "POST",
-    });
-    queryClient.invalidateQueries();
-  };
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className="app">
         <h1>Team Incident Dashboard - Starter Project</h1>
-        <div className="app-actions">
-          <Button onClick={resetData}>Reset data</Button>
-        </div>
         <main>
           <Incidents />
         </main>
