2ea6c1c84d7bcaeb306dfbc10f546ab9b1cf6404
Vitor Leite <vhsleite@gmail.com>
2026-01-15 10:06:55 +0200

    replaced errorwrapper with generic emptystate

--- src/components/ui/ErrorWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./ErrorWrapper.module.css";
-
-interface ErrorWrapperProps {
-  children: React.ReactNode;
-}
-
-export function ErrorWrapper({ children }: ErrorWrapperProps) {
-  return <div className={styles.errorWrapper}>{children}</div>;
-}
